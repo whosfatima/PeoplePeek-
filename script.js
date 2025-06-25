94c5fe8b754e7c1bc8bb59d1b2263132e81d2a5a
maryam <fatimaisherewithyou@gmail.com>
2025-06-25 23:21:54 +0330

    adding elements

---
+++ script.js
@@ -0,0 +1,30 @@
+const users = [
+  { id: 1, name: "Ali", age: 25 },
+  { id: 2, name: "Fatemeh", age: 17 },
+  { id: 3, name: "Negar", age: 30 },
+  { id: 4, name: "Milad", age: 22 },
+  { id: 5, name: "Pari", age: 27 },
+];
+const FilterUser = document.querySelector(".UserContainer");
+const button = document.querySelector('button')
+const input = document.querySelector('input')
+
+const renderUser = (list) => {
+  let str = " ";
+  list.forEach(
+    (user) =>
+      (str += `
+        <div class ='Useritem'>
+        <p><b>Name:</b>${user.name}</p>
+        <p><b>Age:</b>${user.age}</p>
+        </div>
+        `)
+    );
+    FilterUser.innerHTML=str;
+};
+window.onload = () =>{
+    renderUser(users);
+};
+button.onclick = () => {
+    let UserInput = input.value.toLowerCase();
+}
